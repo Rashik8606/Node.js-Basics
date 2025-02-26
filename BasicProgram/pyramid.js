5a9316383963b14af5432a780ffa2ac421efdf56
rashik <mhdrashik112@gmail.com>
2025-02-26 14:37:15 +0530

    Changes

--- BasicProgram/pyramid.js
+++ BasicProgram/pyramid.js
@@ -8,4 +8,42 @@ function  Pyramid () {
     }
 }
 
+function Maths (a,b){
+    let num1 = a+b;
+    let num2 = a-b
+    console.log(num1,num2)
+}
+
 Pyramid()
+Maths(10,40)
+
+
+function List (){
+    var list = [
+        'Rashik',
+        'Farsana',
+        'Mebin',
+        'Akaash',
+        'Vinayak',
+        'Akshai',
+        'Abhinav'
+    ]
+    
+    for (k = 0; k<list.length; k++){
+        console.log(list[k])
+    }
+}
+
+List()
+
+function Largest (){
+    let lar = [10,20,110,40]
+    let max = lar[0]
+    for (i = 1; i<lar.length; i++){
+        if (lar[i]> max){
+            max = lar[i]
+        }
+    }
+    console.log(max)
+}
+Largest()
