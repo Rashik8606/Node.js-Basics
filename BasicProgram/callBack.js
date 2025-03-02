c9452b70bb4366dd4e6cce5a89aae89a9261e47f
rashik <mhdrashik112@gmail.com>
2025-03-02 18:48:42 +0530

    asynchronous and synchronous

---
+++ BasicProgram/callBack.js
@@ -0,0 +1,7 @@
+var details=(name)=>{
+    console.log('name :> '+name)
+}
+var name =(call)=>{
+    call('Rashik')
+}
+name(details)
---
+++ BasicProgram/time dilation.js
@@ -0,0 +1,9 @@
+function LongTime(millsecond){
+    time = new Date()
+    while(new Date-time <=millsecond){
+
+    }
+}
+console.log('Started')
+LongTime(4000)
+console.log('Ended')
